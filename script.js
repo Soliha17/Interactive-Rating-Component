e11006ded55b53a88abfa840e1fd9f90bc8376b9
Soliha <solyatursuntasheva@gmail.com>
2023-02-03 12:18:05 +0500

    Finish!

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+const card = document.querySelector(".card");
+const success = document.querySelector(".success");
 const buttons = document.querySelectorAll(".list__item");
 const submit = document.querySelector(".btn__submit");
 const rating = document.querySelector(".span__value");
-const card = document.querySelector(".card");
-const success = document.querySelector(".success");
 
+submit.disabled = true;
 let value;
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
+    submit.disabled = false;
     buttons.forEach((button) => button.classList.remove("list__change"));
     e.target.classList.add("list__change");
     value = e.target.textContent;
